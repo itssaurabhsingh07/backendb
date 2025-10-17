003a1b723fbe99fee37432f71067d2578cdbbf0e
Saurabh Singh <amreshsingh902681@gmail.com>
2025-10-17 21:04:55 +0530

    Env Files

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ import express from "express";
 const app=express()
 
 app.get("/",(req,res)=>{
-   res.status(200).json({message:"Hello This Side"})
+   res.status(200).json({message:"Hello This Side Saurabh"})
 })
 app.get("/signin",(req,res)=>{
     res.status(200).json({message:"User Login Succesfull"})
 })
 
-const port=3000
+const port=process.env.PORT || 3000
 app.listen(port,()=>{
     console.log("Your Response")
 })
